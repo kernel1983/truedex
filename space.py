df686d4818a52e00ce61340a0f03e40d6ef2582f
KJ <kernel1983@gmail.com>
2026-04-28 15:17:05 -0400

    debug

--- space.py
+++ space.py
@@ -5,6 +5,7 @@ latest_block_number = 0
 states = [{}]  # one state per block
 blocks = {}  # block_number -> [tx_hashes]
 block_hashes = {}  # block_number -> block_hash
+block_times = {}  # block_number -> unix timestamp (seconds)
 transactions = {}  # tx_hash -> tx
 events = {}  # block_number -> [{event, args}, ...]
 nonces = {}  # addr -> nonce
@@ -63,14 +64,33 @@ def event(_event, _args, _block=None):
 def handle_lookup(_addr):
     return _addr
 
-def nextblock():
+def nextblock(slot=None, timestamp=None):
     global states, latest_block_number
-    global blocks, block_hashes
+    global blocks, block_hashes, block_times
+
+    # 如果指定了slot且已存在，不创建新block（同一个slot的多笔交易）
+    if slot is not None and slot in blocks:
+        # 更新时间戳（如果提供了新的）
+        if timestamp is not None:
+            block_times[slot] = timestamp
+        return slot
 
     block_hash = _gen_block_hash()
-    blocks[latest_block_number] = []
-    block_hashes[latest_block_number] = block_hash
+    block_number = slot if slot is not None else latest_block_number
 
-    latest_block_number += 1
+    blocks[block_number] = []
+    block_hashes[block_number] = block_hash
+
+    if timestamp is None:
+        import time
+        timestamp = int(time.time())
+    block_times[block_number] = timestamp
+
+    if slot is None:
+        latest_block_number += 1
+    else:
+        latest_block_number = max(latest_block_number, slot + 1)
+
     states.append({})
+    return block_number
 
